686780b646329f025f76b325e0b0e860b534128a
Oleg Shevelev <mantyr@gmail.com>
2023-08-14 03:52:04 +0200

    v0.0.1

---
+++ a.go
@@ -0,0 +1,9 @@
+package packagea
+
+import (
+	"github.com/mantyr/packageb"
+)
+
+type A struct {
+	B packageb.B
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mantyr/packagea
+
+go 1.19
+
+require github.com/mantyr/packageb v0.0.1
